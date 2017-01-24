320f0a68ad6c2e49fcfc9ac1ec687847359f8b81
Aidan Connolly <aidan.connolly@huskers.unl.edu>
2017-01-24 11:43:08 -0600

    Removing legislature scraper

--- NebraskaLegislatureScraper/bills/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from myproject.bills.models import *
-from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request):
-    bills = Bill.objects.all()
-    return render(request, 'bills/index.html', {'bills':bills})
